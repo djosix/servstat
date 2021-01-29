b7b0cf34ec05950f2530da573edbd04ed2238194
HuJyunKai <contact@sivilization.com>
2021-01-29 20:38:17 +0800

    Update install.sh

--- backend/install.sh
+++ backend/install.sh
@@ -1,4 +1,4 @@
 pip3 install py-cpuinfo gpustat psutil tornado gunicorn
-ln -s /etc/systemd/system/serverstat.service /etc/servstat/backend/serverstat.service
+ln -s /etc/servstat/backend/serverstat.service /etc/systemd/system/serverstat.service 
 systemctl enable --now serverstat
 systemctl start serverstat
